ff27b11dcc2d284b8dbf8e8b41a26cebe36c3087
Ned Zimmerman <ned@bight.ca>
2015-01-07 13:13:42 -0500

    Hide title on front page.

--- content-page.php
+++ content-page.php
@@ -8,7 +8,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3' ); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php if ( !is_front_page() ) : the_title( '<h1 class="entry-title">', '</h1>' ); endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
