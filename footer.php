c54344a0f8091a8e551234b98a46712a76299b44
Ned Zimmerman <ned@bight.ca>
2015-01-13 17:40:56 -0500

    Fixes #9.

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><span class="desktop"><?php bloginfo( 'name' ); ?></span><span class="mobile"><?php echo get_option( 'options_short_title' ); ?></span></a> &copy; 2014&ndash;<?php echo date( 'Y' ); ?><?php if ( get_field( 'mailchimp', 'options' ) ) : ?> | <a href="<?php the_field( 'mailchimp', 'options' ); ?>" title="<?php _e( 'Newsletter', 'gei' ); ?>"></a><?php endif; ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><span class="desktop"><?php bloginfo( 'name' ); ?></span><span class="mobile"><?php echo get_option( 'options_short_title' ); ?></span></a> &copy; 2014&ndash;<?php echo date( 'Y' ); ?><?php if ( get_field( 'mailchimp', 'options' ) ) : ?> | <a href="<?php the_field( 'mailchimp', 'options' ); ?>" title="<?php _e( 'Subscribe to our newsletter', 'gei' ); ?>"><?php _e( 'Newsletter', 'gei' ); ?></a><?php endif; ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
