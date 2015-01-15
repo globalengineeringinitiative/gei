238c42d92c88f396c80f300be7d07014be3cc5c9
Ned Zimmerman <ned@bight.ca>
2015-01-14 21:53:55 -0500

    Library search (fixes #1).

--- sidebar-library.php
+++ sidebar-library.php
@@ -28,4 +28,11 @@
 		<li><a data-filter="<?php echo $term->slug; ?>"><?php echo $term->name; ?></a></li>
 		<?php } ?>
 	</ul>
+	<h1><?php _e( 'Search', 'gei' ); ?></h1>
+	<form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_site_url(); ?>"> 
+	<label class="screen-reader-text" for="s">Search for:</label> 
+	<input type="hidden" value="gei_resource" name="post_type" id="post_type" /> 
+	<input type="text" value="" name="s" id="s" /> 
+	<button type="submit" id="searchsubmit"><i class="fa fa-search"></i><span class="screen-reader-text"> Search</span></button>
+	</form>
 </div><!-- #secondary -->
