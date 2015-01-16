5ad2b153693aa5fe83785f9ac6e084fb5909c8a5
Ned Zimmerman <ned@bight.ca>
2015-01-16 13:46:10 -0500

    Proper search results.

--- search.php
+++ search.php
@@ -19,14 +19,11 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'content', 'search' );
-				?>
+				<?php if ( get_post_type( $post ) == 'gei_resource' ) :
+					get_template_part( 'content', 'resource-brief' );
+				else :
+					get_template_part( 'content', 'search' );
+				endif; ?>
 
 			<?php endwhile; ?>
 
